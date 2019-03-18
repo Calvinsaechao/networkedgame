775c7b43579a0c4a1131b79a59312e96b0f00a2d
CalvinSaechao <calvinsaechao916@gmail.com>
2019-03-18 11:58:40 -0700

    Runnable

--- src/chained/chainedEngine.java
+++ src/chained/chainedEngine.java
@@ -1,30 +1,86 @@
 package chained;
 
+import java.awt.DisplayMode;
+import java.awt.GraphicsEnvironment;
 import java.io.IOException;
 
+import ray.input.GenericInputManager;
 import ray.rage.Engine;
+import ray.rage.game.Game;
 import ray.rage.game.VariableFrameRateGame;
+import ray.rage.rendersystem.RenderSystem;
 import ray.rage.rendersystem.RenderWindow;
+import ray.rage.rendersystem.Viewport;
+import ray.rage.rendersystem.gl4.GL4RenderSystem;
+import ray.rage.scene.Camera;
+import ray.rage.scene.Camera.Frustum.Projection;
+import ray.rml.Vector3f;
 import ray.rage.scene.SceneManager;
+import ray.rage.scene.SceneNode;
 
 public class chainedEngine extends VariableFrameRateGame{
-
+	GL4RenderSystem rs;
+	float elapsTime = 0.0f;
+	private GenericInputManager im;
+	
+	public chainedEngine() {
+		super();
+	}
+	
+	public static void main(String[] args) {
+		Game game = new chainedEngine();
+		try {
+			game.startup();
+			game.run();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		} finally {
+			game.shutdown();
+			game.exit();
+		}
+	}
+	
 	@Override
-	protected void setupCameras(SceneManager arg0, RenderWindow arg1) {
+	protected void setupWindow(RenderSystem rs, GraphicsEnvironment ge) {
+		rs.createRenderWindow(new DisplayMode(1000,700,24,60), false);
+	}
+	
+	protected void setupWindowViewports(RenderWindow rw) {
+		Viewport viewport = rw.getViewport(0);
+		viewport.setDimensions(0f, 0f, 1f, 1f);
+	}
+	
+	@Override
+	protected void setupCameras(SceneManager sm, RenderWindow rw) {
 		// TODO Auto-generated method stub
+		SceneNode rootNode = sm.getRootSceneNode();
+		Camera camera = sm.createCamera("MainCamera", Projection.PERSPECTIVE);
+		camera.setMode('n');
 		
+		rw.getViewport(0).setCamera(camera);
+		
+		camera.setRt((Vector3f)Vector3f.createFrom(1.0f, 0.0f, 0.0f));
+		camera.setUp((Vector3f)Vector3f.createFrom(0.0f, 1.0f, 0.0f));
+		camera.setFd((Vector3f)Vector3f.createFrom(0.0f, 0.0f, -1.0f));
+		camera.setPo((Vector3f)Vector3f.createFrom(0.0f, 0.0f, 0.0f));
+		
+		SceneNode cameraNode = rootNode.createChildSceneNode(camera.getName() + "Node");
+		cameraNode.attachObject(camera);
 	}
 
 	@Override
 	protected void setupScene(Engine arg0, SceneManager arg1) throws IOException {
 		// TODO Auto-generated method stub
+		im = new GenericInputManager();
 		
 	}
 
 	@Override
-	protected void update(Engine arg0) {
+	protected void update(Engine engine) {
 		// TODO Auto-generated method stub
-		
-	}
+		rs = (GL4RenderSystem) engine.getRenderSystem();
+		elapsTime += engine.getElapsedTimeMillis();
+		im.update(elapsTime);
+ 	}
 
 }
