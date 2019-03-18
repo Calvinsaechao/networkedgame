7fd452591d16f46bb184a949ad7c8e24d79b5e52
CalvinSaechao <calvinsaechao916@gmail.com>
2019-03-18 11:31:59 -0700

    setup

---
+++ src/chained/chainedEngine.java
@@ -0,0 +1,30 @@
+package chained;
+
+import java.io.IOException;
+
+import ray.rage.Engine;
+import ray.rage.game.VariableFrameRateGame;
+import ray.rage.rendersystem.RenderWindow;
+import ray.rage.scene.SceneManager;
+
+public class chainedEngine extends VariableFrameRateGame{
+
+	@Override
+	protected void setupCameras(SceneManager arg0, RenderWindow arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setupScene(Engine arg0, SceneManager arg1) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void update(Engine arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
