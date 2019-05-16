673c3715058c344d065fb37a5c6d7d1dfe0310ef
Calvin <calvinsaechao916@gmail.com>
2019-05-16 12:11:09 -0700

    added compile to runClient

--- src/runClient.bat
+++ src/runClient.bat
@@ -1,3 +1,4 @@
+javac -classpath ".;\javagaming\jinput\lib;src;src\*;\javagaming\jbullet\jbullet.jar;\javagaming\jinput\jinput.jar;\javagaming\rage165\ray.jar;\javagaming\vecmath\vecmath.jar;\javagaming\jogamp-all-platforms\jar\gluegen-rt.jar;\javagaming\jogamp-all-platforms\jar\jogl-all.jar" client\*.java ActionClasses\*.java Controllers\*.java InterfaceClasses\*.java server\*.java
 set /p address="Enter Server address:"
 java  -Djava.library.path=\javagaming\jinput\lib -Dsun.java2d.d3d=false -cp ".;\javagaming\jinput\lib;bin\client;bin\client\chainedGame.class;src;src\ActionClasses;src\assets\config;src\client;src\Controllers;src\InterfaceClasses;src\server;\javagaming\jbullet\jbullet.jar;\javagaming\jinput\jinput.jar;\javagaming\rage165\ray.jar;\javagaming\vecmath\vecmath.jar;\javagaming\jogamp-all-platforms\jar\gluegen-rt.jar;\javagaming\jogamp-all-platforms\jar\jogl-all.jar" -Xdiag client.chainedGame %address% 20
 pause
